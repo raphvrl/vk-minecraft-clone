f673c8b21b716c5400f1b1e8d3af043059d7b87c
U-RAPHAEL\rapha <rverrouil@gmail.com>
2025-02-21 23:59:12 +0100

    remove useless files and add gitignore

--- bin/src/utils/console/console.d
+++
@@ -1,4 +0,0 @@
-bin/src/utils/console/console.o: src/utils/console/console.cpp \
- src/utils/console/console.hpp src/core/types.hpp
-src/utils/console/console.hpp:
-src/core/types.hpp:
--- bin/src/utils/drawer/box_drawer.d
+++
@@ -1,4 +0,0 @@
-bin/src/utils/drawer/box_drawer.o: src/utils/drawer/box_drawer.cpp \
- src/utils/drawer/box_drawer.hpp src/core/types.hpp
-src/utils/drawer/box_drawer.hpp:
-src/core/types.hpp:
